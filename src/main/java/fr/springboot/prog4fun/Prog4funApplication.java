26a790e19fad8fb8eea356c4a2d13a40aac89080
ghislain.delestre <delestre.ghislain@gmail.com>
2020-05-06 10:04:28 +0200

    V1 de l'application: DONE (web-services get) update pour déploiement sur AWS

--- src/main/java/fr/springboot/prog4fun/Prog4funApplication.java
+++ src/main/java/fr/springboot/prog4fun/Prog4funApplication.java
@@ -2,9 +2,18 @@ package fr.springboot.prog4fun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+// Pour le déploiement il est necessaire que notre classe hérite de SpringBootServletInitializer. Rajout de la méthode protected.
 @SpringBootApplication
-public class Prog4funApplication {
+public class Prog4funApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Prog4funApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Prog4funApplication.class, args);
